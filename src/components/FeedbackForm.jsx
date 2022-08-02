fcf9fb44ec8804632c2dee745aa496e41491c7de
Nicole Barranca <nikkibarranca@gmail.com>
2022-08-02 15:18:35 -0400

    initial event handler for form input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,13 +3,22 @@ import Card from "./shared/Card";
 
 function FeedbackForm() {
   const [text, settext] = useState("");
+
+  const handleTextChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* @todo - rating select component */}
         <div className="input-group">
-          <input type="text" placeholder="Write a review" />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+          />
           <button type="submit">Send</button>
         </div>
       </form>
