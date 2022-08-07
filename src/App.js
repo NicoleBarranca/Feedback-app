583a7b3c69cd1779bf8ad7886da9182db75be0d7
Nicole Barranca <nikkibarranca@gmail.com>
2022-08-07 16:59:32 -0400

    addFeedback and handleAdd, uuid for feedback items

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ import FeedbackForm from "./components/FeedbackForm";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const addFeedback = (newFeedback) => {
+    console.log(newFeedback);
+  };
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
