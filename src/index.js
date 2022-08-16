4b27c4e51fbbfbcd44c038b87408456058885842
Nicole Barranca <nikkibarranca@gmail.com>
2022-08-16 10:42:20 -0400

    update index.js for react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// ReactDom.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
 import React from "react";
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
